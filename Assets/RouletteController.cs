f22346dc7640eb8a8c6ce56d48722d29bde3ebe0
tsuny221 <kingoftsuny@gmail.com>
2020-06-15 12:00:33 +0900

    [Update] 3-6

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    // Start is called before the first frame updatel
+    float rotSpeed = 0; //回転速度
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+        	this.rotSpeed = 10;
+        }
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        //ルーレットを減速させる（追加）
+        this.rotSpeed *= 0.96f;
+    }
+}
